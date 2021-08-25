3b3b40978e0aac5964c9f0cea09d31468c40f3b6
Benjamin <benjamin.gille@gmail.com>
2021-08-25 10:29:16 +0200

    Ajout gestion landscape

--- lib/page/DetailClient.dart
+++ lib/page/DetailClient.dart
@@ -12,6 +12,7 @@ class DetailClient extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
     return Scaffold(
       appBar: AppBar(
         title: Text('Madera Mobile Application'),
@@ -24,71 +25,146 @@ class DetailClient extends StatelessWidget {
           itemBuilder: (context, index) => Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-              decoration: BoxDecoration(color: Colors.black12),
+              decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+              ),
               child: Center(
                 child: Column(
                   children: [
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      padding:
-                          EdgeInsets.only(left: 0, top: 5, right: 0, bottom: 5),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white),
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            radius: 80,
-                            child: CircleAvatar(
+                    isLandscape ?
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(20),
+                          padding:
+                          EdgeInsets.only(left: 30, top: 5, right: 30, bottom: 5),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(20)),
+                              color: Colors.white),
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 80,
+                                child: CircleAvatar(
+                                  radius: 80,
+                                  child: ClipOval(
+                                    child: Image.network(
+                                      'https://picsum.photos/200',
+                                      errorBuilder: (BuildContext context,
+                                          Object exception,
+                                          StackTrace? stackTrace) {
+                                        return const Icon(Icons.person);
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Center(
+                                child: Text(this.client.first_name,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold, fontSize: 35)),
+                              ),
+                              Center(
+                                child: Text(this.client.mail,
+                                    style: TextStyle(
+                                        height: 2,
+                                        fontSize: 20,
+                                        color: Colors.black38)),
+                              ),
+                              Center(
+                                child: Text(this.client.phone,
+                                    style: TextStyle(
+                                        height: 2,
+                                        fontSize: 20,
+                                        color: Colors.black38)),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(20),
+                          child: Column(
+                            children: [
+                              DetailElement(
+                                  title: 'Adresse', data: this.client.address),
+                              DetailElement(
+                                  title: 'Code postal',
+                                  data: this.client.postal_code),
+                              DetailElement(title: 'Ville', data: this.client.city),
+                              DetailElement(
+                                  title: 'Pays', data: this.client.country),
+                            ],
+                          ),
+                        ),
+                    ],
+                    ):
+                    Column(
+                      children: [
+                        Container(
+                        margin: EdgeInsets.all(20),
+                        padding:
+                        EdgeInsets.only(left: 0, top: 5, right: 0, bottom: 5),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            color: Colors.white),
+                        child: Column(
+                          children: [
+                            CircleAvatar(
                               radius: 80,
-                              child: ClipOval(
-                                child: Image.network(
-                                  'https://picsum.photos/200',
-                                  errorBuilder: (BuildContext context,
-                                      Object exception,
-                                      StackTrace? stackTrace) {
-                                    return const Icon(Icons.person);
-                                  },
+                              child: CircleAvatar(
+                                radius: 80,
+                                child: ClipOval(
+                                  child: Image.network(
+                                    'https://picsum.photos/200',
+                                    errorBuilder: (BuildContext context,
+                                        Object exception,
+                                        StackTrace? stackTrace) {
+                                      return const Icon(Icons.person);
+                                    },
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          Center(
-                            child: Text(this.client.first_name,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 35)),
-                          ),
-                          Center(
-                            child: Text(this.client.mail,
-                                style: TextStyle(
-                                    height: 2,
-                                    fontSize: 20,
-                                    color: Colors.black38)),
-                          ),
-                          Center(
-                            child: Text(this.client.phone,
-                                style: TextStyle(
-                                    height: 2,
-                                    fontSize: 20,
-                                    color: Colors.black38)),
-                          ),
-                        ],
+                            Center(
+                              child: Text(this.client.first_name,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold, fontSize: 35)),
+                            ),
+                            Center(
+                              child: Text(this.client.mail,
+                                  style: TextStyle(
+                                      height: 2,
+                                      fontSize: 20,
+                                      color: Colors.black38)),
+                            ),
+                            Center(
+                              child: Text(this.client.phone,
+                                  style: TextStyle(
+                                      height: 2,
+                                      fontSize: 20,
+                                      color: Colors.black38)),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      child: Column(
-                        children: [
-                          DetailElement(
-                              title: 'Adresse', data: this.client.address),
-                          DetailElement(
-                              title: 'Code postal',
-                              data: this.client.postal_code),
-                          DetailElement(title: 'Ville', data: this.client.city),
-                          DetailElement(
-                              title: 'Pays', data: this.client.country),
-                        ],
-                      ),
+                        Container(
+                          margin: EdgeInsets.all(20),
+                          child: Column(
+                            children: [
+                              DetailElement(
+                                  title: 'Adresse', data: this.client.address),
+                              DetailElement(
+                                  title: 'Code postal',
+                                  data: this.client.postal_code),
+                              DetailElement(title: 'Ville', data: this.client.city),
+                              DetailElement(
+                                  title: 'Pays', data: this.client.country),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                     Map(client: this.client)
                   ],
