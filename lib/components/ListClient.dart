6d56eac890c126c87c560af415dd5b2bb29ae27f
Benjamin <benjamin.gille@gmail.com>
2021-08-25 09:23:56 +0200

    Ajout bouton liste client

--- lib/components/ListClient.dart
+++ lib/components/ListClient.dart
@@ -35,15 +35,6 @@ class _ListClientState extends State<ListClient> {
               child: CircularProgressIndicator(),
             )
           : Column(children: [
-              Container(
-                  padding: const EdgeInsets.all(10),
-                  color: Colors.grey,
-                  child: ListTile(
-                    title: Text("Clients"),
-                    trailing: Icon(Icons.add),
-                    onTap: () =>
-                        Navigator.of(context).pushNamed("/ajoutClient"),
-                  )),
               Expanded(
                 child: ListView.builder(
                   itemCount: clients.length,
@@ -75,6 +66,13 @@ class _ListClientState extends State<ListClient> {
                 ),
               ),
             ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed("/ajoutClient");
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
     );
   }
 }
